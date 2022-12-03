2e037308f475d07a9b15a8ed823a69ccd6d02a41
SerebrovEV <89818121788@mail.ru>
2022-12-03 16:00:16 +0300

    Decided 03/12/2022

---
+++ src/main/java/org/example/Task11.java
@@ -0,0 +1,9 @@
+package org.example;
+//Дан массив строк, необходимо удалить из него все дубли, оставшиеся строки объединить в одну в порядке следования в массиве.
+public class Task11 {
+    private static String[] lists = {"a", "b", "g", "s", "s", "w", "a", "b", "g", "s", "s", "w", "a"};
+
+    public static void main(String[] args){
+        lists
+    }
+}
